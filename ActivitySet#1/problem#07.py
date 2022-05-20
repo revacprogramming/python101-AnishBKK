a877fbc547b20382cef69d9b8ab71ff52cd24529
AnishBKK <94068609+AnishBKK@users.noreply.github.com>
2022-05-20 04:12:03 +0000

    update

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,3 @@
-# Strings
-
 text = "X-DSPAM-Confidence:    0.8475"
+l = text.find("0")
+print(float(text[l:l+7]))
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,15 @@
-# Files
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+tot = 0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    l = (line.find("0"))
+    n = (float(line[l:l+7]))
+    tot = float(tot + n)
+    count = float(count + 1)
 
-filename = "dataset/mbox-short.txt"
+avg = float(tot/count)
+print("Average spam confidence:",avg)
